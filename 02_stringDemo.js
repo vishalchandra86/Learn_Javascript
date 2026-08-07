2626983a01511e7d1308550f4ec0e6620a99736c
vishalchandra86 <vishal.chandra09@gmail.com>
2026-08-07 08:20:52 +0000

    string demo phase 1

---
+++ 02_stringDemo.js
@@ -0,0 +1,14 @@
+// const name="Suresh";
+// const age=24;
+// //console.log(name +age +" ");
+
+// //console.log(`Hello my name is ${name} and my age is about ${age} years`);
+// //console.log("hello".toUpperCase());
+// console.log(`position of character in name variable at position 2 is ${name.at(2)}`);
+// // POSITION OF 'E' IN NAME VARIABLE
+// console.log(`position of first character in name variable is ${name.charAt(0)}`);
+// console.log(`charcode A in ${"ABC".charCodeAt(0)}`);
+//console.log(`${"hello world".indexOf("", 6)}`);
+let text="Hello world, welcome to the javascript  universe.";
+let  result=text[2];
+console.log(result);
