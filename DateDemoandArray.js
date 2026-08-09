73d5142b82c3cf743c75a71e339c45903d7ad1c5
vishalchandra86 <vishal.chandra09@gmail.com>
2026-08-09 07:10:26 +0000

    basic array function done

---
+++ DateDemoandArray.js
@@ -0,0 +1,30 @@
+//**********************Date Demo************************* */
+// const d= new Date();
+// console.log(d.toString());
+
+//=====================Array Demo================================/
+const cars=["BMW","Volvo","Saab","Ford"];
+console.log(`car at position 3 is ${cars[3]}`);
+//change element in array
+cars[0]="Toyota";
+console.log(`car at position 0 after change is ${cars[0]}`);
+console.log(`full element of array car is ${cars}`);
+//array as json
+console.log(`array as json is ${JSON.stringify(cars)}`);
+//looping an array
+// for(let i=0;i<cars.length;i++){
+   
+//     console.log(`car at position ${i} is ${cars[i]}`);
+// }
+
+//using for each loop
+// cars.forEach(function(cars){
+//     console.log(`car is ${cars}`);
+// });
+
+//adding the element in array
+// cars.push("Audi");
+// console.log(`after adding the element in array is ${JSON.stringify(cars)}`);
+
+
+
